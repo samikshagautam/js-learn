09392985d97902294b604228c7a224f0a578481d
Samiksha Gautam <samikshyagautam5@gmail.com>
2019-06-03 21:44:53 -0500

    decoratemytext added

---
+++ decoratemytext.js
@@ -0,0 +1,28 @@
+function decoration() {
+    // alert("Hello, World!");
+
+    //document.getElementById("tarea").style.fontSize="24px";
+    setInterval(function(){
+    var element = document.getElementById("tarea");
+    var computedFontSize = window.getComputedStyle(element, null).getPropertyValue("font-size");
+    var fontSize = parseInt(computedFontSize);
+    element.style.fontSize = (fontSize + 2) + 'px';
+    }, 500);
+}
+
+function dec() {
+    let checkBox = document.getElementById("check");
+    let tarea = document.getElementById("tarea");
+    if (checkBox.checked == true) {
+        /*tarea.style.backgroundImage = "url('http://www.cs.washington.edu/education/courses/190m/CurrentQtr/labs/6/hundred-dollar-bill.jpg')";*/
+        document.getElementById("body").style.backgroundImage = "url('http://www.cs.washington.edu/education/courses/190m/CurrentQtr/labs/6/hundred-dollar-bill.jpg')";
+        tarea.style.fontWeight = "bold";
+        tarea.style.color = " green";
+        tarea.style.textDecoration = "underline";
+    } else {
+        document.getElementById("body").style.backgroundImage = "";
+        tarea.style.fontWeight = "";
+        tarea.style.color = "";
+        tarea.style.textDecoration = "";
+    }
+}
